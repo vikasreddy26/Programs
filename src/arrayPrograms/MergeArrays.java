0e7c2771959bd866f90a44f4aad0bca0b2ed55e8
vikasreddy26 <61445770+vikasreddy26@users.noreply.github.com>
2022-03-25 15:45:07 +0530

    mergecode

---
+++ src/arrayPrograms/MergeArrays.java
@@ -0,0 +1,25 @@
+package arrayPrograms;
+
+import java.util.Arrays;
+
+public class MergeArrays {
+
+	public static void main(String[] args) {
+		int arr [] = {1,2,3};
+		int arr1 [] = {4,5,6};
+		int length = arr.length + arr1.length;
+		int result [] = new int[length];
+		int position = 0;
+		for(int ele:arr) {
+			result[position]=ele;
+			position++;
+		}
+		for(int ele:arr1) {
+			result[position]=ele;
+			position++;
+		}
+		
+		System.out.println(Arrays.toString(result));
+	}
+
+}
